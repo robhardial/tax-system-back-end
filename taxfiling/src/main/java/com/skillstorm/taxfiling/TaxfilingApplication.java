709ace612fd6295f67dfd23b5f1363eb85908758
Rob <roberthardial@gmail.com>
2024-04-13 18:35:48 -0400

    Created spring project and structure

---
+++ taxfiling/src/main/java/com/skillstorm/taxfiling/TaxfilingApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxfiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxfilingApplication.class, args);
+	}
+
+}
---
+++ taxfiling/src/test/java/com/skillstorm/taxfiling/TaxfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxfiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxfilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
