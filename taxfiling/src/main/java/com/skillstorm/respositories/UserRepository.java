fc042f65cf3dbad975a536c460d549fbcb656b9a
Rob <roberthardial@gmail.com>
2024-04-13 20:25:57 -0400

    Created user functionality

---
+++ taxfiling/src/main/java/com/skillstorm/respositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.skillstorm.respositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.skillstorm.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
