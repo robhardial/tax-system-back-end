24d8d5bb56ca80037a3a454c1f6850254047d617
Rob <roberthardial@gmail.com>
2024-04-25 13:20:36 -0400

    Added employer functionality

--- taxfiling/src/main/java/com/skillstorm/controllers/EmployerController.java
+++ taxfiling/src/main/java/com/skillstorm/controllers/EmployerController.java
@@ -2,8 +2,19 @@ package com.skillstorm.controllers;
 
 import com.skillstorm.models.Employer;
 import com.skillstorm.services.EmployerService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +26,34 @@ public class EmployerController {
     @Autowired
     EmployerService employerService;
 
+    @GetMapping
+    public ResponseEntity<List<Employer>> findAllEmployers() {
+        List<Employer> employers = employerService.findAllEmployers();
+        return new ResponseEntity<List<Employer>>(employers, HttpStatus.OK);
+    }
+
+    @GetMapping("/employer/{id}")
+    public ResponseEntity<Employer> getEmployerById(@PathVariable int id) {
+        Employer employer = employerService.findEmployerById(id);
+        return new ResponseEntity<Employer>(employer, HttpStatus.OK);
+    }
+
+    @PostMapping("/employer")
+    public ResponseEntity<Employer> createEmployer(@RequestBody Employer employer) {
+        Employer newEmployer = employerService.saveEmployer(employer);
+        return new ResponseEntity<Employer>(newEmployer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/employer/{id}")
+    public ResponseEntity<Employer> editEmployer(@PathVariable int id, @RequestBody Employer employer) {
+        Employer updatedEmployer = employerService.editEmployer(id, employer);
+        return new ResponseEntity<Employer>(updatedEmployer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/employer/{id}")
+    public ResponseEntity<Employer> deleteEmployer(@PathVariable int id) {
+        employerService.deleteEmployerById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- taxfiling/src/main/java/com/skillstorm/services/EmployerService.java
+++ taxfiling/src/main/java/com/skillstorm/services/EmployerService.java
@@ -1,6 +1,11 @@
 package com.skillstorm.services;
 
+import com.skillstorm.models.Employer;
 import com.skillstorm.respositories.EmployerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +14,42 @@ public class EmployerService {
 
     @Autowired
     EmployerRepository employerRepository;
+
+    public List<Employer> findAllEmployers() {
+        return employerRepository.findAll();
+    }
+
+    public Employer findEmployerById(int id) {
+        Optional<Employer> employer = employerRepository.findById(id);
+
+        if (employer.isPresent()) {
+
+            return employer.get();
+        }
+
+        return null;
+    }
+
+    public Employer saveEmployer(Employer employer) {
+        return employerRepository.save(employer);
+    }
+
+    public Employer editEmployer(int id, Employer employer) {
+        Optional<Employer> existingEmployerOptional = employerRepository.findById(id);
+
+        if (existingEmployerOptional.isPresent()) {
+            Employer existingEmployer = existingEmployerOptional.get();
+            existingEmployer.setEin(employer.getEin());
+            existingEmployer.setName(employer.getName());
+            existingEmployer.setAddress(employer.getAddress());
+            existingEmployer.setFormW2s(employer.getFormW2s());
+            return employerRepository.save(existingEmployer);
+        } else {
+            return employerRepository.save(employer);
+        }
+    }
+
+    public void deleteEmployerById(int id) {
+        employerRepository.deleteById(id);
+    }
 }
