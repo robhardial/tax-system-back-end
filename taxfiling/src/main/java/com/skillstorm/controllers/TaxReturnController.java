d7a13772a8e2a866d56cf0e7548cf9687411d15e
carlosbernal2222 <104038953+carlosbernal2222@users.noreply.github.com>
2024-05-04 16:44:53 -0400

    Added create tax return endpoint in TaxReturnController
    Introduced a new POST endpoint to create a tax return linked to a specific person, identified by their id. The feature checks if the person exists before saving the tax return.

--- taxfiling/src/main/java/com/skillstorm/controllers/TaxReturnController.java
+++ taxfiling/src/main/java/com/skillstorm/controllers/TaxReturnController.java
@@ -1,8 +1,12 @@
 package com.skillstorm.controllers;
 
+import com.skillstorm.models.Person;
 import com.skillstorm.models.TaxReturn;
+import com.skillstorm.services.PersonService;
 import com.skillstorm.services.TaxReturnService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +17,24 @@ public class TaxReturnController {
     @Autowired
     private TaxReturnService taxReturnService;
 
+    @Autowired
+    private PersonService personService;
+
     @GetMapping("/{id}")
     public TaxReturn getTaxReturnById(@PathVariable("id") int id){
         return taxReturnService.getTaxReturnById(id);
     }
+
+    @PostMapping("/{personId}")
+    public ResponseEntity<TaxReturn> createTaxReturn(@PathVariable int personId, @RequestBody TaxReturn taxReturn){
+        Person person = personService.findPersonById(personId);
+        if(person == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        taxReturn.setPerson(person);
+        TaxReturn createdTaxReturn = taxReturnService.save(taxReturn);
+        return new ResponseEntity<>(createdTaxReturn, HttpStatus.CREATED);
+    }
+
 }
